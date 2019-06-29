000e596a99866dff6bbf9852907f2b7bb927cde3
BrunoM24 <brunocostamatos.24@gmail.com>
2019-06-29 17:02:07 +0100

    Added POJOs. Closes #2.

---
+++ src/main/java/com/brunom24/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.brunom24.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/brunom24/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.brunom24.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
