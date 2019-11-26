80c069fc3eba4481b53ceca04eff4612767c7782
BrunoM24 <brunocostamatos.24@gmail.com>
2019-11-26 13:57:59 +0000

    Added Pets and Contact info to Owners on startup. Closes #33

--- pet-clinic-data/src/main/java/com/brunom24/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/brunom24/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.brunom24.sfgpetclinic.services.map;
 
 import com.brunom24.sfgpetclinic.model.Owner;
+import com.brunom24.sfgpetclinic.model.Pet;
 import com.brunom24.sfgpetclinic.services.OwnerService;
+import com.brunom24.sfgpetclinic.services.PetService;
+import com.brunom24.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+	private final PetService petService;
+	private final PetTypeService petTypeService;
+
+	public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+		this.petService = petService;
+		this.petTypeService = petTypeService;
+	}
 
 	@Override
 	public Set<Owner> findAll() {
@@ -26,7 +37,28 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner save(Owner owner) {
-		return super.save(owner);
+		if (owner != null){
+			if (owner.getPets() != null){
+				owner.getPets().forEach(pet -> {
+					if (pet.getPetType() != null){
+						if (pet.getPetType().getId() == null){
+							pet.setPetType(petTypeService.save(pet.getPetType()));
+						}
+					}else {
+						throw new RuntimeException("Pet Type is required");
+					}
+
+					if (pet.getId() == null){
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+
+			return super.save(owner);
+		}else {
+			return null;
+		}
 	}
 
     @Override
